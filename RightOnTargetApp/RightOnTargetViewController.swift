231d3f16988cdb5bc8d39278e5b2a0214c043f46
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-13 18:10:59 -0700

    1. Project created and prepared

---
+++ RightOnTargetApp/RightOnTargetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RightOnTargetApp
+//
+//  Created by OSIIOSdev on 13.03.2025.
+//
+
+import UIKit
+
+final class RightOnTargetViewController: UIViewController {
+
+    // MARK: - Private properties
+    
+    
+    
+    
+    // MARK: - View Lyfecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    // MARK: - Private methods
+    
+}
+

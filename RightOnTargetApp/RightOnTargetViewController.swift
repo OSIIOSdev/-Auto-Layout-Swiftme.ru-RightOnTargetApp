d60a1e56ba215f344cbecb52ebccb07227231f87
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-24 13:12:36 -0700

    1. Model added 2. Business logic transfered to the Model and configured

--- RightOnTargetApp/RightOnTargetViewController.swift
+++ RightOnTargetApp/RightOnTargetViewController.swift
@@ -12,14 +12,16 @@ final class RightOnTargetViewController: UIViewController {
     // MARK: - Private properties
     
     private let desiredValueLabel = UILabel()
+    private let minimumValuelabel = UILabel()
+    private let maximumValueLabel = UILabel()
+    
+    private let sliderStackView = UIStackView()
+    
     private let desiredValueSlider = UISlider()
     private let checkValueButton = UIButton()
     
-    private lazy var value = 0
-    private lazy var round = 0
-    private lazy var points = 0
-    private lazy var result = 0
-    private let maxRoundValue = 10
+    
+    var game: Game!
     
     private let missingValueText = "The value is still missing"
     
@@ -27,31 +29,66 @@ final class RightOnTargetViewController: UIViewController {
     // MARK: - View Lyfecycle
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
         
+        game = Game(minValue: 1, maxValue: 10, rounds: 5, bound: 10)
+        updateLabelWithDesiredValue(textLabel: (game.currentDesiredValue).formatted())
+        
         getValueLabel()
-        getValueSlider()
+        getSliderStackView()
         getCheckedButton()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        
         super.viewDidAppear(false)
         
-        showAlertController(title: "New game", message: "Try to get less than \(maxRoundValue) points. To start a game press \"Start game\"")
+        showAlertController(title: "New game", message: "Try to get less than \(game.limit) points. To start a game press \"Start game\"")
+        
     }
     
     
     // MARK: - Private methods
     
+    private func updateLabelWithDesiredValue(textLabel: String) {
+        desiredValueLabel.text = textLabel
+    }
+    
+    private func getSliderStackView() {
+        
+        view.addSubview(sliderStackView)
+        sliderStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        getValueSlider()
+        
+        [minimumValuelabel, desiredValueSlider, maximumValueLabel].forEach {
+            sliderStackView.addArrangedSubview($0)
+        }
+        
+        sliderStackView.distribution = .fill
+        sliderStackView.alignment = .fill
+        sliderStackView.spacing = 15
+        
+        sliderStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        sliderStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+    }
+    
     private func getValueLabel() {
         
-        desiredValueLabel.textColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-        desiredValueLabel.font = UIFont.boldSystemFont(ofSize: 22)
         desiredValueLabel.text = missingValueText
         
-        view.addSubview(desiredValueLabel)
+        [minimumValuelabel, desiredValueLabel, maximumValueLabel].forEach {
+            view.addSubview($0)
+            
+            $0.textColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            $0.font = UIFont.boldSystemFont(ofSize: 22)
+        }
+        
         desiredValueLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -59,29 +96,25 @@ final class RightOnTargetViewController: UIViewController {
             desiredValueLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100)
         ])
         
+        minimumValuelabel.text = game.minDesiredValue.formatted()
+        maximumValueLabel.text = game.maxDesiredValue.formatted()
+        
     }
     
     private func getValueSlider() {
         
-        desiredValueSlider.minimumValue = 1
-        desiredValueSlider.maximumValue = 50
+        desiredValueSlider.minimumValue = Float(game.minDesiredValue)
+        desiredValueSlider.maximumValue = Float(game.maxDesiredValue)
         desiredValueSlider.value = Float.random(in: desiredValueSlider.minimumValue...desiredValueSlider.maximumValue)
         
         desiredValueSlider.minimumTrackTintColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         desiredValueSlider.maximumTrackTintColor = .white
         desiredValueSlider.thumbTintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-        desiredValueSlider.tintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-        desiredValueSlider.minimumValueImage = UIImage(systemName: "01.square.fill")
-        desiredValueSlider.maximumValueImage = UIImage(systemName: "50.square.fill")
         
         view.addSubview(desiredValueSlider)
         desiredValueSlider.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([
-            desiredValueSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            desiredValueSlider.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
-            desiredValueSlider.widthAnchor.constraint(equalToConstant: 550)
-        ])
+        desiredValueSlider.widthAnchor.constraint(equalToConstant: 550).isActive = true
         
     }
     
@@ -110,31 +143,28 @@ final class RightOnTargetViewController: UIViewController {
     
     @objc func getResultButton() {
         
-        switch round {
-        case 0:
-            value = Int.random(in: Int(desiredValueSlider.minimumValue)...Int(desiredValueSlider.maximumValue))
-            desiredValueLabel.text = value.formatted()
-            
-
+        if checkValueButton.currentTitle == "Start game" {
             checkValueButton.setTitle("Check value", for: .normal)
-            round = 1
-        case 1 ..< 5:
-            points = abs(value - Int(desiredValueSlider.value))
-            result += points
+        }
         
-            value = Int.random(in: Int(desiredValueSlider.minimumValue)...Int(desiredValueSlider.maximumValue))
-            desiredValueLabel.text = value.formatted()
+        game.calculateScore(with: Int(desiredValueSlider.value))
         
-            round += 1
-        default:
-            result <= maxRoundValue ? showAlertController(title: "Game over", message: "You win! Your final result is \(result)") :
-                                      showAlertController(title: "Game over", message: "You lose! Your final result is \(result)")
-            round = 0
-            result = 0
+        if game.isGameOver {
+            game.score <= game.limit ? showAlertController(title: "Game over", message: "You win! Your final result is \(game.score)") :
+            showAlertController(title: "Game over", message: "You lose! Your final result is \(game.score)")
+            
+            game.resdartGame()
+            
             desiredValueLabel.text = 0.formatted()
             checkValueButton.setTitle("Start game", for: .normal)
             desiredValueLabel.text = missingValueText
+            
+            return
+        } else {
+            game.startNewRound()
         }
+        
+        updateLabelWithDesiredValue(textLabel: (game.currentDesiredValue).formatted())
         
     }
     
